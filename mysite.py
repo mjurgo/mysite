432b72dbb1b80bbdbddf1e0244bd09b918a2af78
vonMajkel <mpjurgo@gmail.com>
2021-01-05 17:51:15 +0100

    Move files to module

--- mysite.py
+++ mysite.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template, url_for
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+from mysite import app
 
 if __name__ == '__main__':
     app.run(debug=True)
